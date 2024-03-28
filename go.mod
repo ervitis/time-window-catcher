3ec702a2b98038c1cd2454f3542983b44651a2fa
ervitis <victor.martinalonso@hrbrain.co.jp>
2024-03-28 20:48:26 +0900

    cpu and http checker

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ervitis/time-window-catcher
+
+go 1.22.1
+
+require github.com/mackerelio/go-osstat v0.2.4
--- main.go
+++ main.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand/v2"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
+
+	"github.com/mackerelio/go-osstat/cpu"
 )
 
-func newWindow(per time.Duration) []bool {
-	return make([]bool, 0, int(per.Seconds()))
+func newWindow[T bool | uint64](per time.Duration) []T {
+	return make([]T, 0, int(per.Seconds()))
 }
 
 func startMockServer() {
@@ -60,47 +63,101 @@ const (
 	timeWindow = 60 * time.Second
 )
 
+func httpServerErrCount(window []bool, statusCapt chan bool, errCount *int) {
+	windowRefresh := time.NewTicker(timeWindow)
+
+	for {
+		select {
+		case status := <-statusCapt:
+			window = append(window, status)
+			if !status {
+				*errCount++
+			}
+			if len(window) > cap(window) {
+				oldest := window[0]
+				window = window[1:]
+				if !oldest {
+					*errCount--
+				}
+			}
+			if *errCount > 5 {
+				fmt.Println("server down!")
+				fmt.Println(window)
+				t := 0
+				errCount = &t
+			}
+		case <-windowRefresh.C:
+			window = window[:0]
+			t := 0
+			errCount = &t
+		}
+	}
+}
+
+func cpuProfilerErrCount(errCount *int) {
+	tick := time.NewTicker(2 * time.Second)
+	var latest, before uint64
+	var totalLatest, totalBefore uint64
+
+	for {
+		select {
+		case <-tick.C:
+			stats, err := cpu.Get()
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			latest, totalLatest = stats.User, stats.Total
+			diffTotal := float64(totalLatest - totalBefore)
+			diff := float64(latest-before) / diffTotal * 100
+
+			// I don't understand how the statics are taken
+			// while i am running this script and monitor the cpu stats with 'top' it doesn't reflect well the
+			// cpu stats
+			if before != 0 && diff > 20.0 {
+				if *errCount > 5 {
+					fmt.Println("cpu high!")
+					t := 0
+					errCount = &t
+				}
+				*errCount++
+			}
+			totalBefore = totalLatest
+			before = latest
+			fmt.Println(diff)
+		}
+	}
+}
+
+func cpuTestUsage() {
+	for i := 0; i < 1000000; i++ {
+		for j := 0; j < 100000000; j++ {
+			for k := 0; k < 1000000000; k++ {
+				_ = i*j + k - i + int(math.Pow(2, float64(i)))
+			}
+		}
+	}
+}
+
 func main() {
 	end := make(chan os.Signal, 1)
 	signal.Notify(end, os.Kill, os.Interrupt, syscall.SIGTERM)
 
 	statusCapt := make(chan bool, 10)
-	window := newWindow(timeWindow)
-	windowRefresh := time.NewTicker(timeWindow)
-	var errCount int
+	windowHttp := newWindow[bool](timeWindow)
+
+	var errCountHttp int
+	var errCountCPUProf int
 
 	go startMockServer()
 
 	go checkServer(http.MethodGet, "http://localhost:8080/test", timeWindow, statusCapt)
 
-	go func(errCount *int) {
-		for {
-			select {
-			case status := <-statusCapt:
-				window = append(window, status)
-				if !status {
-					*errCount++
-				}
-				if len(window) > cap(window) {
-					oldest := window[0]
-					window = window[1:]
-					if !oldest {
-						*errCount--
-					}
-				}
-				if *errCount > 5 {
-					fmt.Println("server down!")
-					fmt.Println(window)
-					t := 0
-					errCount = &t
-				}
-			case <-windowRefresh.C:
-				window = window[:0]
-				t := 0
-				errCount = &t
-			}
-		}
-	}(&errCount)
+	go cpuTestUsage()
+
+	go cpuProfilerErrCount(&errCountCPUProf)
+
+	go httpServerErrCount(windowHttp, statusCapt, &errCountHttp)
 
 	<-end
 	fmt.Println("finish")
